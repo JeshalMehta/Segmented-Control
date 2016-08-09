9b0fc19fefcc3eb23794660744291dd970f818ba
Jeshal Mehta <jeshalmehta@jeshals-macbook-pro.local>
2016-08-09 12:44:17 +0530

    Initial Commit

---
+++ SegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegmentedControl
+//
+//  Created by Jeshal Mehta on 09/08/16.
+//  Copyright © 2016 Jeshal Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
